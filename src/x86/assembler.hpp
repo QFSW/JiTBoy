ac97e9b589ecbf0404598d64f3d6e87108534f28
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-10-19 12:49:03 +0100

    Fixed .gitignore

---
+++ src/x86/assembler.hpp
@@ -0,0 +1,497 @@
+#pragma once
+#include <cstdint>
+#include <stdexcept>
+#include <sstream>
+
+#include <vector_buffer.hpp>
+#include <utils/strtools.hpp>
+#include <x86/x86.hpp>
+
+namespace x86
+{
+	enum class InstrMode
+	{
+		RR,
+		RM,
+		MR,
+		IR,
+		IM
+	};
+
+	enum class JumpAdjust
+	{
+		None,
+		Always,
+		Auto
+	};
+
+	class Assembler
+	{
+	public:
+		void reset();
+		void copy(void* ptr) const;
+		[[nodiscard]] size_t size() const noexcept;
+		[[nodiscard]] std::string get_debug() const;
+
+		#pragma region Non Immediate Instructions
+		
+		template <Opcode Op, InstrMode Mode = InstrMode::RR, RegisterSize Size = RegisterSize::Reg32>
+		void instr(Register dst, Register src);
+
+		template <Opcode Op, InstrMode Mode, RegisterSize Size = RegisterSize::Reg32>
+		void instr(Register dst, Register src, int32_t addr_offset);
+
+		template <Opcode Op, OpcodeExt Ext, InstrMode Mode = InstrMode::RR, RegisterSize Size = RegisterSize::Reg32>
+		void instr(Register reg);
+
+		template <Opcode Op, OpcodeExt Ext, InstrMode Mode = InstrMode::RR, RegisterSize Size = RegisterSize::Reg32>
+		void instr(Register reg, int32_t addr_offset);
+
+		template <Opcode Op>
+		void instr()
+		{
+			_buffer.write(Op);
+			if constexpr (debug) _debug_stream << opcode_to_string(Op) << "\n";
+		}
+		
+		#pragma endregion 
+
+		#pragma region Immediate Instructions
+		
+		template <Opcode Op, OpcodeExt Ext, InstrMode Mode = InstrMode::IR, RegisterSize Size = RegisterSize::Reg32>
+		void instr_imm(Register dst, uint32_t imm);
+
+		template <Opcode Op, OpcodeExt Ext, InstrMode Mode = InstrMode::IR, RegisterSize Size = RegisterSize::Reg32>
+		void instr_imm(Register dst, uint32_t imm, int32_t addr_offset);
+
+		#pragma endregion
+
+		#pragma region Jump Instructions
+
+		template <JumpAdjust Adjust = JumpAdjust::Auto>
+		void jump(int32_t offset);
+
+		template <CondCode Cond, JumpAdjust Adjust = JumpAdjust::Auto>
+		void jump_cond(int32_t offset);
+
+		template <JumpAdjust Adjust = JumpAdjust::Auto>
+		void call(int32_t offset);
+		
+		#pragma endregion
+
+		#pragma region Misc Intructions
+
+		template <CondCode Cond, InstrMode Mode = InstrMode::RR, RegisterSize Size = RegisterSize::Reg32>
+		void move_cond(Register dst, Register src);
+
+		template <CondCode Cond, InstrMode Mode, RegisterSize Size = RegisterSize::Reg32>
+		void move_cond(Register dst, Register src, int32_t addr_offset);
+
+		void bswap(Register dst);
+		void enter(uint16_t size, uint8_t nesting = 0);
+		
+		#pragma endregion 
+
+	private:
+		VectorBuffer _buffer;
+
+		template <Opcode Op, InstrMode Mode, RegisterSize Size>
+		static constexpr uint8_t encode_opcode();
+
+		template <Opcode Op, RegisterSize Size>
+		static constexpr uint8_t encode_imm_opcode(uint32_t imm);
+
+		template <Opcode Op, InstrMode Mode, RegisterSize Size>
+		void write_opcode();
+
+		template <Opcode Op, RegisterSize Size>
+		void write_imm_opcode(uint32_t imm);
+
+		template <Opcode Op, RegisterSize Size>
+		void write_immediate(uint32_t imm);
+
+		void encode_regs(RegisterMode mode, Register dst, Register src);
+		void encode_regs_offset(Register dst, Register src, int32_t addr_offset);
+
+		template <JumpAdjust Adjust>
+		static constexpr int32_t adjust_offset(int32_t offset, uint8_t instr_size);
+
+		template <Opcode Op>
+		static constexpr bool is_fast_imm_instr();
+
+		template <JumpAdjust Adjust, uint8_t NearSize = 2>
+		static constexpr bool is_near_jump(int32_t offset);
+
+		static constexpr bool is_8_bit(int32_t val);
+
+		static constexpr bool debug = true;
+		std::stringstream _debug_stream;
+	};
+
+	#pragma region Non Immediate Instruction Implementations
+
+	template <Opcode Op, InstrMode Mode, RegisterSize Size>
+	void Assembler::instr(const Register dst, const Register src)
+	{
+		write_opcode<Op, Mode, Size>();
+		
+		if constexpr (Mode == InstrMode::RR) encode_regs(RegisterMode::Register, dst, src);
+		else if constexpr (Mode == InstrMode::RM) encode_regs(RegisterMode::MemoryDisp0, dst, src);
+		else if constexpr (Mode == InstrMode::MR) encode_regs(RegisterMode::MemoryDisp0, src, dst);
+		else throw std::logic_error("Invalid instruction mode encountered");
+
+		if constexpr (debug)
+		{
+			_debug_stream << opcode_to_string(Op) << " ";
+
+			const auto reg1 = reg_to_string(dst, Size);
+			const auto reg2 = reg_to_string(src, Size);
+
+			using namespace strtools;
+			if constexpr (Mode == InstrMode::RR) _debug_stream << catf("%s %s", reg1, reg2);
+			else if constexpr (Mode == InstrMode::RM) _debug_stream << catf("[%s] %s",reg1, reg2);
+			else if constexpr (Mode == InstrMode::MR) _debug_stream << catf("%s [%s]",reg1, reg2);
+			
+			_debug_stream << "\n";
+		}
+	}
+
+	template <Opcode Op, InstrMode Mode, RegisterSize Size>
+	void Assembler::instr(const Register dst, const Register src, const int32_t addr_offset)
+	{
+		write_opcode<Op, Mode, Size>();
+		
+		if constexpr (Mode == InstrMode::RM) encode_regs_offset(dst, src, addr_offset);
+		else if constexpr (Mode == InstrMode::MR) encode_regs_offset(src, dst, addr_offset);
+		else throw std::logic_error("Invalid instruction mode encountered");
+
+		if constexpr (debug)
+		{
+			_debug_stream << "???\n";
+		}
+	}
+
+	template <Opcode Op, OpcodeExt Ext, InstrMode Mode, RegisterSize Size>
+	void Assembler::instr(const Register reg)
+	{
+		instr<Op, Mode, Size>(reg, static_cast<Register>(Ext));
+
+		if constexpr (debug)
+		{
+			_debug_stream << "???\n";
+		}
+	}
+
+	template <Opcode Op, OpcodeExt Ext, InstrMode Mode, RegisterSize Size>
+	void Assembler::instr(const Register reg, const int32_t addr_offset)
+	{
+		instr<Op, Mode, Size>(reg, static_cast<Register>(Ext), addr_offset);
+
+		if constexpr (debug)
+		{
+			_debug_stream << "???\n";
+		}
+	}
+
+	#pragma endregion
+
+	#pragma region Immediate Instruction Implementations
+
+	template <Opcode Op, OpcodeExt Ext, InstrMode Mode, RegisterSize Size>
+	void Assembler::instr_imm(const Register dst, const uint32_t imm)
+	{
+		write_imm_opcode<Op, Size>(imm);
+
+		if constexpr (Mode == InstrMode::IR) encode_regs(RegisterMode::Register, dst, static_cast<Register>(Ext));
+		else if constexpr (Mode == InstrMode::IM) encode_regs(RegisterMode::MemoryDisp0, dst, static_cast<Register>(Ext));
+		else throw std::logic_error("Invalid instruction mode encountered");
+
+		write_immediate<Op, Size>(imm);
+
+		if constexpr (debug)
+		{
+			_debug_stream << opcode_imm_to_string(Op, Ext) << " ";
+
+			const auto reg = reg_to_string(dst, Size);
+
+			using namespace strtools;
+			if constexpr (Mode == InstrMode::IR) _debug_stream << catf("%s %d", reg, imm);
+			else if constexpr (Mode == InstrMode::IM) _debug_stream << catf("[%s] %d", reg, imm);
+
+			_debug_stream << "\n";
+		}
+	}
+
+	template <Opcode Op, OpcodeExt Ext, InstrMode Mode, RegisterSize Size>
+	void Assembler::instr_imm(const Register dst, const uint32_t imm, const int32_t addr_offset)
+	{
+		write_imm_opcode<Op, Size>(imm);
+
+		if constexpr (Mode == InstrMode::IM) encode_regs_offset(dst, static_cast<Register>(Ext), addr_offset);
+		else throw std::logic_error("Invalid instruction mode encountered");
+
+		write_immediate<Op, Size>(imm);
+
+		if constexpr (debug)
+		{
+			_debug_stream << "???\n";
+		}
+	}
+
+	#pragma endregion 
+
+	#pragma region Jump Instruction Implementations
+
+	template <JumpAdjust Adjust>
+	constexpr int32_t Assembler::adjust_offset(const int32_t offset, const uint8_t instr_size)
+	{
+		if constexpr (Adjust == JumpAdjust::Always) return offset - instr_size;
+		else if constexpr (Adjust == JumpAdjust::Auto)
+		{
+			if (offset < 0) return offset - instr_size;
+		}
+
+		return offset;
+	}
+
+	template <JumpAdjust Adjust>
+	void Assembler::jump(int32_t offset)
+	{
+		const bool is_near = is_near_jump<Adjust>(offset);
+		const uint8_t instr_size = 1 + (is_near ? 1 : 4);
+		
+		offset = adjust_offset<Adjust>(offset, instr_size);
+
+		if (is_near)
+		{
+			_buffer.write(JMP_8);
+			_buffer.write<int8_t>(offset);
+		}
+		else
+		{
+			_buffer.write(JMP_32);
+			_buffer.write<int32_t>(offset);
+		}
+
+		if constexpr (debug)
+		{
+			_debug_stream << "???\n";
+		}
+	}
+
+	template <CondCode Cond, JumpAdjust Adjust>
+	void Assembler::jump_cond(int32_t offset)
+	{
+		const bool is_near = is_near_jump<Adjust>(offset);
+		const uint8_t instr_size = 1 + (is_near ? 1 : 5);
+
+		offset = adjust_offset<Adjust>(offset, instr_size);
+
+		if (is_near)
+		{
+			constexpr uint8_t op = static_cast<uint8_t>(Cond) | static_cast<uint8_t>(Jcc_8);
+			
+			_buffer.write(op);
+			_buffer.write<int8_t>(offset);
+		}
+		else
+		{
+			constexpr uint8_t op = static_cast<uint8_t>(Cond) | static_cast<uint8_t>(Jcc_32);
+			
+			_buffer.write(OpcodePrefix::Jcc_32);
+			_buffer.write(op);
+			_buffer.write<int32_t>(offset);
+		}
+
+		if constexpr (debug)
+		{
+			_debug_stream << strtools::catf("J%s %d\n", cond_to_string(Cond), offset);
+		}
+	}
+
+	template <JumpAdjust Adjust>
+	void Assembler::call(int32_t offset)
+	{
+		offset = adjust_offset<Adjust>(offset, 5);
+
+		_buffer.write(CALL);
+		_buffer.write<int32_t>(offset);
+
+		if constexpr (debug)
+		{
+			_debug_stream << strtools::catf("CALL %d\n", offset);
+		}
+	}
+
+	#pragma endregion 
+
+	#pragma region Misc Instruction Implementations
+
+	template <CondCode Cond, InstrMode Mode, RegisterSize Size>
+	void Assembler::move_cond(const Register dst, const Register src)
+	{
+		constexpr auto op = static_cast<Opcode>(static_cast<uint8_t>(CMOVcc) | static_cast<uint8_t>(Cond));
+
+		if constexpr (Size == RegisterSize::Reg8) throw std::logic_error("CMOVcc does not support 8 bit operands");
+		else if constexpr (Size == RegisterSize::Reg16) _buffer.write(OpcodePrefix::Size16);
+
+		_buffer.write(OpcodePrefix::CMOVcc);
+
+		if constexpr (Mode == InstrMode::RR) instr<op, InstrMode::RR, RegisterSize::Reg8>(src, dst);
+		else if constexpr (Mode == InstrMode::MR) instr<op, InstrMode::RM, RegisterSize::Reg8>(src, dst);
+		else throw std::logic_error("Unsupported instruction mode");
+
+		if constexpr (debug)
+		{
+			_debug_stream << strtools::catf("CMOV%s ", cond_to_string(Cond));
+
+			const auto reg1 = reg_to_string(dst, Size);
+			const auto reg2 = reg_to_string(src, Size);
+
+			using namespace strtools;
+			if constexpr (Mode == InstrMode::RR) _debug_stream << catf("%s %s", reg1, reg2);
+			else if constexpr (Mode == InstrMode::MR) _debug_stream << catf("%s [%s]", reg1, reg2);
+
+			_debug_stream << "\n";
+		}
+	}
+
+	template <CondCode Cond, InstrMode Mode, RegisterSize Size>
+	void Assembler::move_cond(const Register dst, const Register src, const int32_t addr_offset)
+	{
+		constexpr auto op = static_cast<Opcode>(static_cast<uint8_t>(CMOVcc) | static_cast<uint8_t>(Cond));
+
+		if constexpr (Size == RegisterSize::Reg8) throw std::logic_error("CMOVcc does not support 8 bit operands");
+		else if constexpr (Size == RegisterSize::Reg16) _buffer.write(OpcodePrefix::Size16);
+
+		_buffer.write(OpcodePrefix::CMOVcc);
+
+		if constexpr (Mode == InstrMode::MR) instr<op, InstrMode::RM, RegisterSize::Reg8>(src, dst, addr_offset);
+		else throw std::logic_error("Unsupported instruction mode");
+
+		if constexpr (debug)
+		{
+			_debug_stream << "???\n";
+		}
+	}
+
+	#pragma endregion
+
+	template <Opcode Op, InstrMode Mode, RegisterSize Size>
+	constexpr uint8_t Assembler::encode_opcode()
+	{
+		auto op = static_cast<uint8_t>(Op);
+		switch (Size)
+		{
+			case RegisterSize::Reg16:
+			case RegisterSize::Reg32: op |= 0b1; break;
+			default: break;
+		}
+
+		switch (Mode)
+		{
+			case InstrMode::MR: op |= 0b10; break;
+			default: break;
+		}
+
+		return op;
+	}
+
+	template <Opcode Op, RegisterSize Size>
+	constexpr uint8_t Assembler::encode_imm_opcode(const uint32_t imm)
+	{
+		auto opcode = static_cast<uint8_t>(Op);
+		if constexpr (Size == RegisterSize::Reg8)
+		{
+			return opcode;
+		}
+		else
+		{
+			opcode |= 0b1;
+			if constexpr (is_fast_imm_instr<Op>())
+			{
+				if (is_8_bit(imm))
+				{
+					opcode |= 0b10;
+				}
+			}
+
+			return opcode;
+		}
+	}
+
+	template <Opcode Op>
+	constexpr bool Assembler::is_fast_imm_instr()
+	{
+		switch (Op)
+		{
+		case MOV_I:
+		case CALL:
+		case RET:
+			return false;
+		default:
+			return true;
+		}
+	}
+
+	template <JumpAdjust Adjust, uint8_t NearSize>
+	constexpr bool Assembler::is_near_jump(const int32_t offset)
+	{
+		if constexpr (Adjust == JumpAdjust::None)
+		{
+			return is_8_bit(offset);
+		}
+		else if constexpr (Adjust == JumpAdjust::Auto)
+		{
+			return offset >= -128 + NearSize && offset <= 127;
+		}
+		else
+		{
+			return offset >= -128 + NearSize && offset <= 127 + NearSize;
+		}
+	}
+
+	template <Opcode Op, InstrMode Mode, RegisterSize Size>
+	void Assembler::write_opcode()
+	{
+		constexpr uint8_t opcode = encode_opcode<Op, Mode, Size>();
+		if constexpr (Size == RegisterSize::Reg16)
+		{
+			_buffer.write(OpcodePrefix::Size16);
+		}
+
+		_buffer.write(opcode);
+	}
+
+	template <Opcode Op, RegisterSize Size>
+	void Assembler::write_imm_opcode(const uint32_t imm)
+	{
+		const uint8_t opcode = encode_imm_opcode<Op, Size>(imm);
+		if constexpr (Size == RegisterSize::Reg16)
+		{
+			_buffer.write(OpcodePrefix::Size16);
+		}
+
+		_buffer.write(opcode);
+	}
+
+	template <Opcode Op, RegisterSize Size>
+	void Assembler::write_immediate(const uint32_t imm)
+	{
+		if constexpr (Size == RegisterSize::Reg8) _buffer.write<uint8_t>(imm);
+		else
+		{
+			constexpr bool fast = is_fast_imm_instr<Op>();
+			if (fast && is_8_bit(imm)) _buffer.write<uint8_t>(imm);
+			else if constexpr (Size == RegisterSize::Reg16) _buffer.write<uint16_t>(imm);
+			else if constexpr (Size == RegisterSize::Reg32) _buffer.write<uint32_t>(imm);
+			else throw std::logic_error("Unsupported register size");
+		}
+	}
+
+	constexpr bool Assembler::is_8_bit(const int32_t val)
+	{
+		return val == static_cast<int8_t>(val);
+	}
+}
+
---
+++ src/x86/x86.hpp
@@ -0,0 +1,215 @@
+#pragma once
+#include <cstdint>
+
+// ReSharper disable CppInconsistentNaming
+// ReSharper disable IdentifierTypo
+
+namespace x86
+{
+	enum Opcode : uint8_t
+	{
+		ADD = 0x00,
+		ADC = 0x10,
+		SUB = 0x29,
+		MOV = 0x88,
+		INC = 0xFE,
+		DEC = 0xFE,
+		NEG = 0xF6,
+		CMP = 0x38,
+		MUL = 0xF6,
+		DIV = 0xF6,
+
+		AND = 0x20,
+		OR  = 0x08,
+		XOR = 0x30,
+		NOT = 0xF6,
+
+		PUSH = 0xFF,
+		POP  = 0x8F,
+
+		NOP = 0x90,
+
+		ADD_I = 0x80,
+		ADC_I = 0x80,
+		SUB_I = 0x80,
+		MOV_I = 0xC6,
+		AND_I = 0x80,
+		OR_I  = 0x80,
+		XOR_I = 0x80,
+		CMP_I = 0x80,
+
+		ADD_I_EAX = 0x50,
+
+		RET   = 0xC3,
+		ENTER = 0xC8,
+		LEAVE = 0xC9,
+
+		JMP_8  = 0xEB,
+		JMP_32 = 0xE9,
+		CALL   = 0xE8,
+
+		CMOVcc = 0x40,
+		Jcc_8  = 0x70,
+		Jcc_32 = 0x80
+	};
+
+	enum class OpcodeExt : uint8_t
+	{
+		INC  = 0,
+		DEC  = 1,
+		NEG  = 3,
+		NOT  = 2,
+		PUSH = 6,
+		POP  = 0,
+		MUL  = 4,
+		DIV  = 6,
+
+		ADD_I = 0,
+		ADC_I = 2,
+		SUB_I = 5,
+		MOV_I = 0,
+		AND_I = 4,
+		OR_I  = 1,
+		XOR_I = 6,
+		CMP_I = 7
+	};
+
+	enum class OpcodePrefix : uint8_t
+	{
+		Size16 = 0x66,
+		Size64 = 0x48,
+		Lock   = 0xF0,
+		CMOVcc = 0x0F,
+		Jcc_32 = 0x0F
+	};
+
+	enum class RegisterSize : uint8_t
+	{
+		Reg8  = 8,
+		Reg16 = 16,
+		Reg32 = 32,
+		Reg64 = 64
+	};
+
+	enum class RegisterMode : uint8_t
+	{
+		MemoryDisp0 = 0b00 << 6,
+		MemoryDisp1 = 0b01 << 6,
+		MemoryDisp4 = 0b10 << 6,
+		Register    = 0b11 << 6
+	};
+
+	enum class CondCode : uint8_t
+	{
+		A   = 0x07,
+		AE  = 0x03,
+		B   = 0x02,
+		BE  = 0x06,
+		C   = 0x02,
+		E   = 0x04,
+		G   = 0x0F,
+		GE  = 0x0D,
+		L   = 0x0C,
+		LE  = 0x0E,
+		NA  = 0x06,
+		NAE = 0x02,
+		NB  = 0x03,
+		NBE = 0x07,
+		NC  = 0x03,
+		NE  = 0x05,
+		NG  = 0x0E,
+		NGE = 0x0C,
+		NL  = 0x0D,
+		NLE = 0x0F,
+		NO  = 0x01,
+		NP  = 0x0B,
+		NS  = 0x09,
+		NZ  = 0x05,
+		O   = 0x00,
+		P   = 0x0A,
+		PE  = 0x0A,
+		PO  = 0x0B,
+		S   = 0x08,
+		Z   = 0x04
+	};
+
+	enum class Register : uint8_t
+	{
+		RAX  = 0b000,
+		RCX  = 0b001,
+		RDX  = 0b010,
+		RBX  = 0b011,
+		RSP  = 0b100,
+		RBP  = 0b101,
+		RSI  = 0b110,
+		RDI  = 0b111,
+
+		R8   = 0b1000,
+		R9   = 0b1001,
+		R10  = 0b1010,
+		R11  = 0b1011,
+		R12  = 0b1100,
+		R13  = 0b1101,
+		R14  = 0b1110,
+		R15  = 0b1111,
+
+		EAX  = 0b000,
+		ECX  = 0b001,
+		EDX  = 0b010,
+		EBX  = 0b011,
+		ESP  = 0b100,
+		EBP  = 0b101,
+		ESI  = 0b110,
+		EDI  = 0b111,
+
+		R8D  = 0b1000,
+		R9D  = 0b1001,
+		R10D = 0b1010,
+		R11D = 0b1011,
+		R12D = 0b1100,
+		R13D = 0b1101,
+		R14D = 0b1110,
+		R15D = 0b1111,
+
+		AX   = 0b000,
+		CX   = 0b001,
+		DX   = 0b010,
+		BX   = 0b011,
+		SP   = 0b100,
+		BP   = 0b101,
+		SI   = 0b110,
+		DI   = 0b111,
+
+		R8W  = 0b1000,
+		R9W  = 0b1001,
+		R10W = 0b1010,
+		R11W = 0b1011,
+		R12W = 0b1100,
+		R13W = 0b1101,
+		R14W = 0b1110,
+		R15W = 0b1111,
+
+		AL   = 0b000,
+		CL   = 0b001,
+		DL   = 0b010,
+		BL   = 0b011,
+		AH   = 0b100,
+		CH   = 0b101,
+		DH   = 0b110,
+		BH   = 0b111,
+
+		R8L  = 0b1000,
+		R9L  = 0b1001,
+		R10L = 0b1010,
+		R11L = 0b1011,
+		R12L = 0b1100,
+		R13L = 0b1101,
+		R14L = 0b1110,
+		R15L = 0b1111
+	};
+
+	const char* opcode_to_string(Opcode op);
+	const char* opcode_imm_to_string(Opcode op, OpcodeExt ext);
+	const char* reg_to_string(Register reg, RegisterSize size);
+	const char* cond_to_string(CondCode cond);
+}
