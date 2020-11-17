5c66358dd465133de01ebbf36775625b5d47a452
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-11-17 23:29:29 +0000

    Added pressure test

---
+++ tests/mips/basic/addi_2.s
@@ -0,0 +1,10 @@
+; desc: adds over multiple registers
+;
+; init: $1 = 0
+; assert: $1 == 10
+; assert: $2 == 15
+; assert: $3 == 16
+
+addi $1 $1 10
+addi $2 $1 5
+addi $3 $2 1
---
+++ tests/mips/misc/pressure.s
@@ -0,0 +1,68 @@
+; name: register pressure
+; desc: utilizes every register
+;
+; assert: $0 == 0
+; assert: $1 == 1
+; assert: $2 == 2
+; assert: $3 == 3
+; assert: $4 == 4
+; assert: $5 == 5
+; assert: $6 == 6
+; assert: $7 == 7
+; assert: $8 == 8
+; assert: $9 == 9
+; assert: $10 == 10
+; assert: $11 == 11
+; assert: $12 == 12
+; assert: $13 == 13
+; assert: $14 == 14
+; assert: $15 == 15
+; assert: $16 == 16
+; assert: $17 == 17
+; assert: $18 == 18
+; assert: $19 == 19
+; assert: $20 == 20
+; assert: $21 == 21
+; assert: $22 == 22
+; assert: $23 == 23
+; assert: $24 == 24
+; assert: $25 == 25
+; assert: $26 == 26
+; assert: $27 == 27
+; assert: $28 == 28
+; assert: $29 == 29
+; assert: $30 == 30
+; assert: $31 == 31
+
+addi $0 $0 0
+addi $1 $0 1
+addi $2 $0 2
+addi $3 $0 3
+addi $4 $0 4
+addi $5 $0 5
+addi $6 $0 6
+addi $7 $0 7
+addi $8 $0 8
+addi $9 $0 9
+addi $10 $0 10
+addi $11 $0 11
+addi $12 $0 12
+addi $13 $0 13
+addi $14 $0 14
+addi $15 $0 15
+addi $16 $0 16
+addi $17 $0 17
+addi $18 $0 18
+addi $19 $0 19
+addi $20 $0 20
+addi $21 $0 21
+addi $22 $0 22
+addi $23 $0 23
+addi $24 $0 24
+addi $25 $0 25
+addi $26 $0 26
+addi $27 $0 27
+addi $28 $0 28
+addi $29 $0 29
+addi $30 $0 30
+addi $31 $0 31
