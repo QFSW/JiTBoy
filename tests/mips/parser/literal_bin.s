fe1c7a1ede2319a9d720e385529ad58189dd6819
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-12-01 23:26:18 +0000

    Hex and binary literals are now tested

---
+++ tests/mips/parser/literal_bin.s
@@ -0,0 +1,4 @@
+# desc: tests binary literals in assembly
+# assert: $1 == 256
+
+addi $1, $0, 0b100000000
---
+++ tests/mips/parser/literal_hex.s
@@ -0,0 +1,4 @@
+# desc: tests hex literals in assembly
+# assert: $1 == 256
+
+addi $1, $0, 0x100
