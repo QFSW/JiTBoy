b098d3ea467ad2e17343490435b3e17ea9e7a323
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-12-23 20:19:16 +0000

    Added overflow tests

---
+++ tests/mips/basic/addiu_4.s
@@ -0,0 +1,7 @@
+# desc: overflow
+#
+# init: $1 = 0xFFFFFFFF
+# init: $2 = 23
+# assert: $3 == 22
+
+addu $3, $2, $1
---
+++ tests/mips/basic/addu_6.s
@@ -0,0 +1,6 @@
+# desc: overflow
+#
+# init: $1 = 0xFFFFFFFF
+# assert: $3 == 22
+
+addiu $3, $1, 23
