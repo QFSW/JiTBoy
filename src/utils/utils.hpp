2d788dc9137c2e03a46cb9abe9ac92976a868d9b
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-12-25 11:57:35 +0000

    Fixed nameof<T>

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -42,6 +42,18 @@ namespace utils
         return typeid(T).name();
     }
 
+    template<> constexpr const char* nameof<uint8_t>() { return "uint8_t"; }
+    template<> constexpr const char* nameof<uint16_t>() { return "uint16_t"; }
+    template<> constexpr const char* nameof<uint32_t>() { return "uint32_t"; }
+    template<> constexpr const char* nameof<uint64_t>() { return "uint64_t"; }
+
+    template<> constexpr const char* nameof<int8_t>() { return "int8_t"; }
+    template<> constexpr const char* nameof<int16_t>() { return "int16_t"; }
+    template<> constexpr const char* nameof<int32_t>() { return "int32_t"; }
+    template<> constexpr const char* nameof<int64_t>() { return "int64_t"; }
+
+    template<> constexpr const char* nameof<std::string>() { return "std::string"; }
+
     template <typename T>
     void swap(T& t1, T& t2) noexcept
     {
