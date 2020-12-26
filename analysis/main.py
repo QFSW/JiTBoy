1e0d7f910c77b4fa8738ee9a57e4705872928493
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-12-26 14:11:20 +0000

    More analysis

--- analysis/main.py
+++ analysis/main.py
@@ -1,12 +1,23 @@
-from loader import load_data
-from plot import scatter
+import loader
+import plot
 
 data_path = "output/results.csv"
+output_base = "output/graphs"
+
+def draw_scatters(data):
+    base = "%s/scatter" % output_base
+    plot.scatter(data, 'hotness', 'mips', '%s/hotness.png' % base, xscale='log', yscale='log')
+    plot.scatter(data, 'source block size', 'efficiency c', '%s/efficiency.png' % base, xscale='log', yscale='linear')
+
+def draw_histograms(data):
+    base = "%s/histogram" % output_base
+    plot.histogram(data, 'time (us)', '%s/time.png' % base, yscale="log")
+    plot.histogram(data, 'mips', '%s/mips.png' % base, yscale="log")
 
 def main():
-    data = load_data(data_path)
-    scatter(data, 'hotness', 'mips', 'output/graphs/hotness.png', xscale='log', yscale='log')
-    scatter(data, 'source block size', 'efficiency c', 'output/graphs/efficiency.png', xscale='log', yscale='linear')
+    data = loader.load_data(data_path)
+    draw_scatters(data)
+    draw_histograms(data)
 
 if __name__ == "__main__":
     main()
--- analysis/plot.py
+++ analysis/plot.py
@@ -1,7 +1,16 @@
+import os
 import matplotlib.pyplot as plt
 import matplotlib.style as style
 
 style.use('ggplot')
+
+def savefig(path):
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    plt.savefig(path)
+    plt.clf()
 
 def scatter(data, x, y, path, xscale='linear', yscale='linear'):
     xdata = list(map(lambda i: i[x], data))
@@ -12,5 +21,13 @@ def scatter(data, x, y, path, xscale='linear', yscale='linear'):
     plt.xscale(xscale)
     plt.yscale(yscale)
     plt.plot(xdata, ydata, 'x', color='black')
-    plt.savefig(path)
-    plt.clf()
+    savefig(path)
+
+def histogram(data, x, path, bins=30, yscale='linear'):
+    xdata = list(map(lambda i: i[x], data))
+
+    plt.xlabel(x)
+    plt.ylabel("Count")
+    plt.yscale(yscale)
+    plt.hist(xdata, bins=bins)
+    savefig(path)
