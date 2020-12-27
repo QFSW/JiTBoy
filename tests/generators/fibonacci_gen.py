248300febbff86f8b05a0a5491b2a6a0b1299ebb
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-12-27 12:48:57 +0000

    Added fibonacci generator

---
+++ tests/generators/fibonacci_gen.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+tpl_path = "tests/generators/fibonacci_tpl.s"
+path = "tests/mips/perf/recursion/"
+tests = 15
+
+def fib(n):
+    if (n <= 1):
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+def main():
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+    tpl = ""
+    with open(tpl_path, 'r') as f:
+        tpl = f.read()
+
+    for i in range(tests):
+        with open("%sfibonacci_%02d.s" % (path, i), 'w') as f:
+            test_input = str(i)
+            test_output = str(fib(i))
+            test = tpl
+            test = test.replace('$INPUT', test_input)
+            test = test.replace('$OUTPUT', test_output)
+            f.write(test)
+
+if __name__ == "__main__":
+    main()
---
+++ tests/mips/perf/recursion/fibonacci_14.s
@@ -0,0 +1,61 @@
+# name: fibonacci(14)
+# desc: recursive fibonacci using stack, computes fib(14)
+#
+# init: $sp = 0x20000000
+# init: $6 = 14
+# assert: $5 == 377
+
+setup:
+    jal fibonacci
+    nop
+
+    j end
+
+
+# $5 is output, $6 is input
+fibonacci:
+    fib_init:
+        addiu $sp, $sp, -4
+        sw $ra, 0($sp)
+
+        addi $1, $6, -1
+        bgtz $1, fib_rec
+        nop
+
+    fib_base:
+        addiu $5, $6, 0
+        j fib_return
+        nop
+
+    fib_rec:
+        addiu $sp, $sp, -4
+        sw $6, 0($sp)
+
+        # fib(n - 1)
+        addiu $6, $6, -1
+        jal fibonacci
+        nop
+
+        # store fib(n - 1)
+        lw $6, 0($sp)
+        sw $5, 0($sp)
+
+        # fib(n - 2)
+        addiu $6, $6, -2
+        jal fibonacci
+        nop
+
+        lw $6, 0($sp)
+        addiu $sp, $sp, 4
+
+        # fib(n - 1) + fib(n - 2)
+        add $5, $5, $6
+
+    fib_return:
+        lw $ra, 0($sp)
+        addiu $sp, $sp, 4
+
+        jr $ra
+        nop
+
+end:
