c163f6ac941d50ef242339c38ab7aecdff375103
Yusuf Ismail <yusuf@qfsw.co.uk>
2021-05-03 16:39:26 +0100

    Delay slot BGTZ tests

--- tests/mips/branch/delay_bgtz_1.s
+++ tests/mips/branch/delay_bgtz_1.s
@@ -1,14 +1,16 @@
-# desc: bgtz not taken
+# desc: bgtz not taken with delay slot
 #
 # init: $1 = 0
 # init: $3 = 0
 # init: $4 = 0
+# init: $5 = 0
 #
 # assert: $3 == 1
 # assert: $4 == 1
+# assert: $5 == 1
 
 bgtz $1, skip
-nop
+addi $5, $5, 1
 addi $3, $0, 1
 
 skip:
