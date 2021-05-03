9bcc9546740ad5d5884546c1b3e698d7572e004c
Yusuf Ismail <yusuf@qfsw.co.uk>
2021-05-03 16:58:26 +0100

    BLTZ delay slot tests

--- tests/mips/branch/delay_bltz_2.s
+++ tests/mips/branch/delay_bltz_2.s
@@ -1,14 +1,16 @@
-# desc: bltz not taken
+# desc: bltz not taken with delay slot
 #
 # init: $1 = 1
 # init: $3 = 0
 # init: $4 = 1
+# init: $5 = 0
 #
 # assert: $3 == 1
 # assert: $4 == 1
+# assert: $5 == 1
 
 bltz $1, skip
-nop
+addi $5, $5, 1
 addi $3, $0, 1
 
 skip:
