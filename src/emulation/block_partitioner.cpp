3e528bd12842ab24a09f1105b54786190ee6d1bc
Yusuf Ismail <yusuf@qfsw.co.uk>
2021-05-05 18:44:35 +0100

    Block partitioner now includes delay slot instruction

--- src/emulation/block_partitioner.cpp
+++ src/emulation/block_partitioner.cpp
@@ -10,8 +10,9 @@ namespace emulation
         if (!program.valid_addr(addr)) [[unlikely]]
             throw std::logic_error(strtools::catf("Address 0x%x is out of bounds", addr));
 
-        const size_t start_index = (addr - program.start_addr) / 4;
-        uint32_t end_index = start_index;
+        size_t start_index = (addr - program.start_addr) / 4;
+        size_t end_index = start_index;
+
         for (; end_index < program.source.size(); end_index++)
         {
             const auto& instr = program.source[end_index];
@@ -21,6 +22,9 @@ namespace emulation
                 break;
             }
         }
+
+        // Include delay slot instruction
+        end_index = std::min(end_index + 4, program.source.size());
 
         const auto code = std::span<const mips::Instruction>(program.source.data() + start_index, end_index - start_index);
         return SourceBlock(code, addr);
