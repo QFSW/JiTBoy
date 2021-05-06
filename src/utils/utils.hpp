2d442aa205ca097bf66a2c54a2abd4162e25fe99
Yusuf Ismail <yusuf@qfsw.co.uk>
2021-05-07 00:51:33 +0100

    Fixed bitmask utils

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -111,12 +111,18 @@ namespace utils
     template <typename T>
     constexpr T create_low_bitmask(const uint8_t bits)
     {
+        if (bits == sizeof(T) * 8)
+            return ~0;
+
         return ~0 ^ (T(~0) << bits);
     }
 
     template <typename T>
     constexpr T create_high_bitmask(const uint8_t bits)
     {
+        if (bits == 0)
+            return 0;
+
         uint8_t shamt = 8 * sizeof(T) - bits;
         return T(~0) << shamt;
     }
