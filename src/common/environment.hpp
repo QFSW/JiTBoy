31aefd8f36865e474eb135c5228a7750238bb74f
Yusuf Ismail <yusuf@qfsw.co.uk>
2021-05-09 15:50:08 +0100

    Singleton improvements

--- src/common/environment.hpp
+++ src/common/environment.hpp
@@ -9,10 +9,16 @@ namespace common
     public:
         threading::ThreadPool& thread_pool() { return _thread_pool; }
 
+        Environment(const Environment&)    = delete;
+        Environment(Environment&&)         = delete;
+        void operator=(const Environment&) = delete;
+        void operator=(Environment&&)      = delete;
+
         static Environment& get();
 
     private:
-        Environment() = default;
+        Environment()  = default;
+        ~Environment() = default;
 
         threading::ThreadPool _thread_pool;
     };
